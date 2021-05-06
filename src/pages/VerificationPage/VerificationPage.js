475a359df891f5611ce8e124aa73f63de6c6633f
Debadrita438 <bristibandyopadhyay@gmail.com>
2021-05-06 21:23:23 +0530

    Implemented Firebase utility, included ability to store authenticated users into our Firestore

--- src/pages/VerificationPage/VerificationPage.js
+++ src/pages/VerificationPage/VerificationPage.js
@@ -1,10 +1,12 @@
 import SignIn from '../../components/SignIn/SignIn';
+import SignUp from '../../components/SignUp/SignUp';
 
 import './VerificationPage.scss';
 
 const VerificationPage = () => (
     <div className='verification'>
         <SignIn />
+        <SignUp />
     </div>
 );
 
