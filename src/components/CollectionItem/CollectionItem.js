e99172dc04c6bd003f699e694d43c08b3479d944
Debadrita438 <bristibandyopadhyay@gmail.com>
2021-05-20 11:00:51 +0530

    Updated shopData with new data, updated cartUtils to group items in cart reducer

--- src/components/CollectionItem/CollectionItem.js
+++ src/components/CollectionItem/CollectionItem.js
@@ -1,18 +1,33 @@
+import { connect } from 'react-redux';
+
+import { addItem } from '../../redux/cart/cartAction';
+import CustomButton from '../CustomButton/CustomButton';
+
 import './CollectionItem.scss';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
-    <div className="collection-item">
-        <div
-            className='image'
-            style={{
-                backgroundImage: `url(${imageUrl})`
-            }}
-        />
-        <div className="collection-footer">
-            <span className="name">{ name }</span>
-            <span className="price">{ price }</span>
-        </div>
-    </div> 
-);
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+});
+
+const CollectionItem = ({ item, addItem }) => {
+    const { name, price, imageUrl } = item;
+    return (
+        <div className="collection-item">
+            <div
+                className='image'
+                style={{
+                    backgroundImage: `url(${imageUrl})`
+                }}
+            />
+            <div className="collection-footer">
+                <span className="name">{ name }</span>
+                <span className="price">{ price }</span>
+            </div>
+            <CustomButton onClick={ () => addItem(item) } inverted>
+                Add to cart
+            </CustomButton>
+        </div> 
+    )
+};
  
-export default CollectionItem;
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/redux/cart/cartAction.js
+++ src/redux/cart/cartAction.js
@@ -2,4 +2,9 @@ import { CartActionType } from './cartActionType';
 
 export const toggleCartHidden = () => ({
     type: CartActionType.TOGGLE_CART_HIDDEN
-})
+});
+
+export const addItem = item => ({
+    type: CartActionType.ADD_ITEM,
+    payload: item
+});
