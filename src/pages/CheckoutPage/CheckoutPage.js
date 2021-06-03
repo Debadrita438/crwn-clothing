0a3e92442fefc8320f770c45bcac50a2d9318e5f
macbook <macbook@macbooks-MacBook-Pro.local>
2021-06-03 19:13:49 +0530

    Converting all styles to styled components

--- src/pages/CheckoutPage/CheckoutPage.js
+++ src/pages/CheckoutPage/CheckoutPage.js
@@ -6,7 +6,13 @@ import CheckoutItem from '../../components/CheckoutItem/CheckoutItem';
 import StripeButton from '../../components/StripeButton/StripeButton';
 // import StripeCheckoutButton from '../../components/StripeButton/StripeCheckoutButton';
 
-import './CheckoutPage.scss';
+import { 
+    CheckoutHeaderBlockContainer, 
+    CheckoutHeaderContainer, 
+    CheckoutPageContainer, 
+    CheckoutTotal, 
+    CheckoutWarning 
+} from './CheckoutPage.styles';
 
 
 const mapStateToProps = createStructuredSelector({
@@ -16,40 +22,40 @@ const mapStateToProps = createStructuredSelector({
 
 const CheckoutPage = ({ cartItems, total }) => {
     return (
-        <div className='checkout-page'>
-           <div className="checkout-header">
-               <div className="header-block">
+        <CheckoutPageContainer>
+           <CheckoutHeaderContainer>
+               <CheckoutHeaderBlockContainer>
                    <span>Product</span>
-               </div>
-               <div className="header-block">
+               </CheckoutHeaderBlockContainer>
+               <CheckoutHeaderBlockContainer>
                    <span>Description</span>
-               </div>
-               <div className="header-block">
+               </CheckoutHeaderBlockContainer>
+               <CheckoutHeaderBlockContainer>
                    <span>Quantity</span>
-               </div>
-               <div className="header-block">
+               </CheckoutHeaderBlockContainer>
+               <CheckoutHeaderBlockContainer>
                    <span>Price</span>
-               </div>
-               <div className="header-block">
+               </CheckoutHeaderBlockContainer>
+               <CheckoutHeaderBlockContainer>
                    <span>Remove</span>
-               </div>
-           </div>
+               </CheckoutHeaderBlockContainer>
+           </CheckoutHeaderContainer>
            {
                cartItems.map(cartItem => 
                     <CheckoutItem key={ cartItem.id } cartItem={ cartItem } />
                 )
            }
-           <div className="total">TOTAL: ₹{ total }</div>
-           <div className='test-warning'>
+           <CheckoutTotal>TOTAL: ₹{ total }</CheckoutTotal>
+           <CheckoutWarning>
                *Please use the following test credit card for the payments*
                <br />
                Card No. - 4242 4242 4242 4242 (Visa Card)
                <br />
                Exp Date - 09/24 CVV - 123
-           </div>
+           </CheckoutWarning>
            <StripeButton price={ total } />
            {/* <StripeCheckoutButton price={ total } /> */}
-        </div>
+        </CheckoutPageContainer>
     );
 }
  
