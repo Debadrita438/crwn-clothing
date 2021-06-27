d8386debbc619d5965ad5fa6f300402d2300d4c5
Debadrita438 <bristibandyopadhyay@gmail.com>
2021-06-27 10:42:58 +0530

    Updating our app to be PWA

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
 import { Provider } from 'react-redux'; 
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorkerRegistration';
 import { store, persistor } from './redux/store';
 
 import { IndexGlobalStyles } from './global.styles';
@@ -23,6 +24,8 @@ ReactDOM.render(
     </Provider>,
   document.getElementById('root')
 );
+
+serviceWorker.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
