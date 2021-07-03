d3f2378461ba4a18c152cc6a29e5637c0a668431
Debadrita438 <bristibandyopadhyay@gmail.com>
2021-07-03 10:43:53 +0530

    Fixing mobile view

--- client/src/components/MenuItem/MenuItem.styles.jsx
+++ client/src/components/MenuItem/MenuItem.styles.jsx
@@ -56,6 +56,10 @@ export const ContentContainer = styled.div`
     background-color: white;
     opacity: 0.7;
     position: absolute;
+
+    @media screen and (max-width: 800px) {
+        width: 60px;
+    }
 `;
 
 export const TitleStyles = styled.h1`
