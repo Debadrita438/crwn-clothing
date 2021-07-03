ca452a4d51c5097abc3f62c6da35f0ad3620ad6b
Debadrita438 <bristibandyopadhyay@gmail.com>
2021-07-03 21:07:14 +0530

    Fix issues about icons

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(compression());
     app.use(enforce.HTTPS({ trustProtoHeader: true }));
     app.use(express.static(path.join(__dirname, 'client/build')));
 
